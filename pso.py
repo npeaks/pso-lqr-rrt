2c3a24ae1ca4b36dc97579b3a4fc6a2ac202dc55
Barry Anthony McNamara III <bamazap@hotmail.com>
2018-04-24 18:05:45 -0400

    Wrote power cost function

--- pso.py
+++ pso.py
@@ -2,25 +2,57 @@
 import numpy as np
 import pyswarms as ps
 
+# must be higher than worst possible cost without it (3)
+penalty_constant = 5
+
 num_trajectories = 10
 num_waypoints = 100
 num_coordinates = 3
+
+time_between_waypoints = .1
+
 altitude_coordinate = 2
-max_altitude = 10
-min_altitude = 0
+
 start_coords = np.array([[0, 0, 0]])
 end_coords = np.array([[1, 1, 1]])
 
+# choose based on environment
+max_altitude = 10
+min_altitude = 0
+
+# properties of the Earth
+air_density = 1.292 # roughly sea level, room temperature
+gravitational_acceleration = 9.81
+
+# properties of robot (bebop2)
+robot_mass = 0.5
+drag_coefficient = 0.25 # estimate for quadcopter
+robot_area = 1/3 * .328 * .382 # assuming 1/3 cover of top-down dimensions
+
+# estimate motor force given bebop2 max vertical speed is 6 m/s
+def air_resistance(velocity):
+    return .5 * air_density * drag_coefficient * robot_area * \
+        np.square(velocity)
+
+max_force = robot_mass * gravitational_acceleration + air_resistance(6)
+
+
 # trajectory is ndarray with shape (num_waypoints, num_coordinates)
 
+
+# Length cost function
 def length(trajectory):
-    diffs = trajectory[:-1] - trajectory[1:]
-    distances = np.apply_along_axis(np.linalg.norm, 1, diffs)
+    differences = trajectory[1:] - trajectory[:-1]
+    distances = np.apply_along_axis(np.linalg.norm, 1, differences)
     return np.sum(distances)
 
 def cost_length(trajectory):
-    return 1 - np.linalg.norm(end_coords - start_coords) / length(trajectory)
+    length_line = np.linalg.norm(end_coords - start_coords)
+    length_actual = length(trajectory)
+    return 1 - length_line / length_actual
 
+
+# Altitude cost function
 def cost_altitude(trajectory):
     if altitude_coordinate == None:
         return 0
@@ -28,21 +60,61 @@ def cost_altitude(trajectory):
     avg_altitude = np.sum(altitudes) / num_waypoints
     return (avg_altitude - min_altitude) / (max_altitude - min_altitude)
 
+
+# Danger zone cost function
 def cost_dangerzones(trajectory):
     return 0 # TODO
 
-def cost_power(trajectory):
-    return 0 # TODO
+# Power cost function
+def gravitational_force(shape):
+    F_g = np.zeros(shape)
+    F_g[:, altitude_coordinate] = np.ones(shape[0])
+    F_g *= robot_mass * gravitational_acceleration
+    return F_g
 
+def length_nonfeasible(trajectory):
+    # calculate dynamics
+    position_differences = trajectory[1:] - trajectory[:-1]
+    velocities = position_differences / time_between_waypoints
+    velocity_differences = velocities[1:] - velocities[:-1]
+    accelerations = velocity_differences / time_between_waypoints
+    # make shapes match
+    accelerations = np.vstack((accelerations, \
+        np.zeros((1, accelerations.shape[1]))))
+    # calculate forces
+    forces = robot_mass * accelerations + air_resistance(velocities) + \
+        gravitational_force(accelerations.shape)
+    # find where too much force is used
+    overall_forces = np.apply_along_axis(np.linalg.norm, 1, forces)
+    mask_nonfeasible = overall_forces > max_force
+    # calculate length of infeasible part of trajectory
+    distances = np.apply_along_axis(np.linalg.norm, 1, position_differences)
+    return np.sum(distances * mask_nonfeasible)
+
+
+def cost_power(trajectory):
+    L_nonfeasible = length_nonfeasible(trajectory)
+    if L_nonfeasible:
+        return penalty_constant + L_nonfeasible / length(trajectory)
+    return 0
+
+
+# Collision cost function
 def cost_collision(trajectory):
     return 0 # TODO
 
+
+# Fuel cost function
 def cost_fuel(trajectory):
     return 0 # TODO
 
+
+# Smoothing cost function
 def cost_smoothing(trajectory):
     return 0 # TODO
 
+
+# Overall cost function
 def cost_total_trajectory(trajectory):
     return cost_length(trajectory) + cost_altitude(trajectory) + \
         cost_dangerzones(trajectory) + cost_power(trajectory) + \
